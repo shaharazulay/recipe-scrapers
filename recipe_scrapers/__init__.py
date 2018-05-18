e2ee47ac466b0baf38d9c1e469e8b5343fb63b56
Tali Raveh-Sadka <tali.sadka@gmail.com>
2018-05-18 12:57:16 -0700

    trying separating results from recipe scraper object generation, for mongo storage

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -114,20 +114,36 @@ class AsyncScraper(object):
                     stream=stream)
                 for url in url_paths]
 
-        scrapers = []
-        for f, u in zip(futures, url_paths):
+        results = []
+        for f in futures:
             try:
                 r = f.result()
-                s = SCRAPERS[url_path_to_dict(u)['host']](r)
             except Exception as e:
                 # Tmp: add logging here of failure
                 print("FAILURE: %s", e)
-                s = SCRAPERS[url_path_to_dict(u)['host']](None)
-            scrapers.append(s)
+                r = None
+            results.append(r)
 
         session.close()
         print(datetime.datetime.now())
+        return results
+
+    def create_recipe_scrapers(self, url_paths, server_responses):
+        scrapers = []
+        for r, u in zip(server_responses, url_paths):
+            try:
+                s = SCRAPERS[url_path_to_dict(u)['host']](r)
+            except Exception as e:
+                # Tmp: add logging here of failure
+                print("FAILED TO CREATE SCRAPER: %s", e)
+                s = SCRAPERS[url_path_to_dict(u)['host']](None)
+            scrapers.append(s)
+
         return scrapers
 
+    def get_recipe_scrapers(self, url_paths, timeout=300, stream=False, use_proxy=False):
+        results = self.get(url_paths, timeout=300, stream=False, use_proxy=False)
+        return self.create_recipe_scrapers(url_paths, results)
 
+    
 __all__ = ['AsyncScraper']
