961a4a7e7914c1184cafcbc8dc3474873450e374
Tali Raveh-Sadka <tali.sadka@gmail.com>
2018-05-22 12:08:01 -0700

    added foodnetwrok, untested

--- recipe_scrapers/foodnetwork.py
+++ recipe_scrapers/foodnetwork.py
@@ -6,36 +6,36 @@ class FoodNetwork(AbstractScraper):
 
     @classmethod
     def host(self):
-        return 'foodnetwork.com'
+        return ['foodnetwork.com']
 
     def title(self):
-        return self.soup.find('h1').get_text().strip()
+        return self.soup.findAll('span', {'class': 'o-AssetTitle__a-HeadlineText'}).get_text()
 
     def total_time(self):
-        return get_minutes(self.soup.find(
-            'dd',
-            {'class': 'o-RecipeInfo__a-Description--Total'})
-        )
+        return get_minutes(self.soup.find('dd', {'class': 'o-RecipeInfo__a-Description--Total'}))
 
     def ingredients(self):
-        ingredients = self.soup.findAll(
-            'li',
-            {'class': 'o-Ingredients__a-ListItem'}
-        )
+        ingredients_html = self.soup.findAll('label', {'class': 'o-Ingredients__a-ListItemText'})
 
         return [
             normalize_string(ingredient.get_text())
-            for ingredient in ingredients
+            for ingredient in ingredients_html
+            if ingredient.get_text(strip=True) not in ('Add all ingredients to list', '')
         ]
 
     def instructions(self):
-        instructions = self.soup.find(
-            'div',
-            {'class': 'o-Method__m-Body'}
-        ).findAll('p')
+        instructions_html = self.soup.findAll('div', {'class': 'o-Method__m-Body'})
 
         return '\n'.join([
             normalize_string(instruction.get_text())
-            for instruction in instructions
-            if instruction.get_text(strip=True) not in ('Watch how to make this recipe.', )
+            for instruction in instructions_html
         ])
+
+    def categories(self):
+        categories_html = self.soup.findAll('a', {'class': 'o-Capsule__a-Tag a-Tag'})
+
+        return [
+            normalize_string(category.get_text())
+            for category in categories_html
+        ]
+    
